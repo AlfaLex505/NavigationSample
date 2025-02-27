5f5eff2ca30e9a4cf16a79648e010e4047b4bd79
Alexis Alfaro <alexisalfaro505@gmail.com>
2025-02-27 16:52:37 -0600

    Add the composables for the first screen and the preview.

--- app/src/main/java/com/example/navigationsample/MainActivity.kt
+++ app/src/main/java/com/example/navigationsample/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.navigationsample.ui.theme.NavigationSampleTheme
@@ -29,3 +31,15 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun FirstScreen(){
+    val name = remember {
+        mutableStateOf("")
+    }
+}
+
+@Preview
+@Composable
+fun FirstPreview(){
+
+}
