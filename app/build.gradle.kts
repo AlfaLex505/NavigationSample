b3391e7ef2d98ee4693ce13976680f326fe1c6aa
Alexis Alfaro <alexisalfaro505@gmail.com>
2025-02-27 20:30:40 -0600

    Change the grade version to the programs runs.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.navigationsample"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.navigationsample"
