6e6d26aa1b2746ad615ca69e0c4d8970c983740d
Alexis Alfaro <alexisalfaro505@gmail.com>
2025-02-27 20:31:38 -0600

    Set a first screen and the preview for it

--- app/src/main/java/com/example/navigationsample/MainActivity.kt
+++ app/src/main/java/com/example/navigationsample/MainActivity.kt
@@ -3,15 +3,25 @@ package com.example.navigationsample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.navigationsample.ui.theme.NavigationSampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    FirstScreen()
                 }
             }
         }
@@ -36,10 +46,32 @@ fun FirstScreen(){
     val name = remember {
         mutableStateOf("")
     }
+
+//    Adding a column
+    Column (modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally)
+    {
+        Text("This is the first screen", fontSize = 24.sp)
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(value = name.value, onValueChange = {
+            name.value = it
+        })
+
+//        Adding a button
+        Button(onClick = { /*TODO*/ }) {
+            Text("Go to the second screen!")
+        }
+
+
+    }
+
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun FirstPreview(){
-
+    FirstScreen()
 }
