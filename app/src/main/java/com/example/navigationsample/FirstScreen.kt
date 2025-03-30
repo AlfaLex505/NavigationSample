9696df91bef7805a4efbf774c21d39b07fe9420e
Alexis Alfaro <alexisalfaro505@gmail.com>
2025-03-30 16:40:49 -0600

    Add fields and function ot the first screen

--- app/src/main/java/com/example/navigationsample/FirstScreen.kt
+++ app/src/main/java/com/example/navigationsample/FirstScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -19,8 +20,11 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun FirstScreen(navigationToSecondScreen:(String)->Unit){
+fun FirstScreen(navigationToSecondScreen:(String, Int)->Unit){
     val name = remember {
+        mutableStateOf("")
+    }
+    val age = remember {
         mutableStateOf("")
     }
 
@@ -36,9 +40,12 @@ fun FirstScreen(navigationToSecondScreen:(String)->Unit){
         OutlinedTextField(value = name.value, onValueChange = {
             name.value = it
         })
+        OutlinedTextField(value = age.value, onValueChange = {
+            age.value = it
+        })
 
 //        Adding a button
-        Button(onClick = { navigationToSecondScreen(name.value) }) {
+        Button(onClick = { navigationToSecondScreen(name.value, age.value.toInt()) }) {
             Text("Go to the second screen!")
         }
 
@@ -47,8 +54,8 @@ fun FirstScreen(navigationToSecondScreen:(String)->Unit){
 
 }
 
-@Preview(showBackground = true)
-@Composable
-fun FirstPreview(){
-    FirstScreen({})
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun FirstPreview(){
+//    FirstScreen({})
+//}
